9df646920750c28826d8e6f8c9bb0c79c9239a08
NAYALAYA <student.171773@worcesterschools.net>
2026-04-30 13:41:19 -0400

    Committed

---
+++ main.py
@@ -0,0 +1,35 @@
+# List of books
+
+
+
+# Print welcome message
+
+
+
+# Print Menu
+
+
+
+# Ask user what they wanna do
+
+
+
+# Ask for book
+
+
+
+# Add it to list
+
+
+
+# Print success message
+
+
+
+# if "show"
+
+
+
+# Print out list
+
+
