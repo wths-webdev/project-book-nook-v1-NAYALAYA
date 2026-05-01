3b2c8959620c65d7fe5c8377a4de56b8250d2b63
NAYALAYA <student.171773@worcesterschools.net>
2026-05-01 11:36:10 -0400

    Book Nook

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # List of books
 
-
+books = []
 
 # Print welcome message
 
+print("\nWelcome to...\n"
+    "      ______ ______\n"
+    "    _/      Y      \\_\n"
+    "   // ~Book | ~~ ~  \\\n"
+    "  // ~ ~ ~~ |  Nook~ \\\n"
+    " //________.|.________\\\n"
+    "`----------`-'----------'\n")
 
 
 # Print Menu
 
-
+print("\nMenu:\n" 
+ "Add book (add)\n"
+ "Remove book (remove) \n"
+ "Show inventory count (count)\n" 
+ "Quit (q)\n")
 
 # Ask user what they wanna do
 
-
+Demand = input("What would you like to do:")
 
 # Ask for book
 
