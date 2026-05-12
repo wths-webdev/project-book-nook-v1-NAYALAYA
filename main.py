03c039b1b94fa401bece0fd3ac3684319f19e7b4
NAYALAYA <student.171773@worcesterschools.net>
2026-05-12 13:01:57 -0400

    Completed

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while True:
     if ask == "add":
 
         new_book = input("What book would you like to add?")
-        Books.append(new_book)
+        Books.append(new_book.strip().lower())
 
         print(new_book, "has been added to your list.")
 
@@ -44,10 +44,13 @@ while True:
     if ask == "remove":
     
         removal = input("What book would you like to remove?")
-        Books.remove(removal)
-      
-        print(removal, "has been removed from your list.")
+        Books.remove(removal.strip().lower())
     
+    # If user requests to remove a book that doesn't already exist in their list print out an apology statement
+
+        if removal not in Books:
+            print("Sorry, it appears you do not own this book.")
+
         for b in Books:
             print(b)
 
